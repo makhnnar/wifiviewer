6af2667f084e03f8e5f139102e0f0608c07aac84
Pedro Gomez <makhnnar@gmail.com>
2018-08-13 10:43:53 -0400

    Creating layouts

--- app/src/main/java/com/testapplication/makhnnar/wifiviewerinit/home/MainActivity.java
+++ app/src/main/java/com/testapplication/makhnnar/wifiviewerinit/home/MainActivity.java
@@ -1,7 +1,9 @@
-package com.testapplication.makhnnar.wifiviewerinit;
+package com.testapplication.makhnnar.wifiviewerinit.home;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.testapplication.makhnnar.wifiviewerinit.R;
 
 public class MainActivity extends AppCompatActivity {
 
